ccea1fedc7e84db5b0db719eb626be7bd33dbfde
me42th <david.meth@hotmail.com>
2024-11-07 17:10:32 -0300

    migrations

--- database/migrations/2024_11_07_200556_create_post_tag_table.php
+++ database/migrations/2024_11_07_200556_create_post_tag_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamps();
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
         });
     }
 
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_tag');
     }
 };
